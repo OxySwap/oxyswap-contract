b5ae37821c45d516c5a40047b247c1b80296a80a
leo <leochan007@163.com>
2020-11-14 07:36:40 +0800

    remove codes.

--- contracts/OxyChef.sol
+++ contracts/OxyChef.sol
@@ -10,7 +10,7 @@ Unless those projects have severe vulnerabilities, this contract will be fine
 
 */
 
-pragma solidity ^0.6.12;
+pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
